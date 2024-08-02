c774546437f5c7c86c42f273517a57d003207193
PC <yeonwoo_lee3@tmax.co.kr>
2024-08-03 01:01:14 +0900

    feat: 폴링 봇 추가

---
+++ src/main/java/com/yanoos/telegram_bot/Bot.java
@@ -0,0 +1,21 @@
+package com.yanoos.telegram_bot;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class Bot extends TelegramLongPollingBot {
+
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {}
+
+}
